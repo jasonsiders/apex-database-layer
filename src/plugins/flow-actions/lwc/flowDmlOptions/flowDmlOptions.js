812e43d73b15d770e05f5ef6cebb244f614b69c6
Jason Siders <jgsiders@gmail.com>
2026-03-18 09:19:28 -0500

    fix custom property editor LWC compilation errors
    - Remove is-required={false} attribute usage (LWC disallows boolean literals in templates)
    - Change is-required={true} to is-required="true" (string coercion)
    - Update flowDmlSection.connectedCallback to handle string "true" via truthy check
    - Remove invalid lightning__FlowCustomPropertyEditor target from metadata (CPEs are referenced by configurationEditor annotation only, no special target needed)

--- src/plugins/flow-actions/lwc/flowDmlOptions/flowDmlOptions.js
+++ src/plugins/flow-actions/lwc/flowDmlOptions/flowDmlOptions.js
@@ -1,65 +1,65 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api } from "lwc";
 
 const DEFAULT_ASSIGNMENT_RULE = { assignmentRuleId: null, useDefaultRule: false };
 const DEFAULT_DUPLICATE_RULE = { allowSave: false, runAsCurrentUser: false };
 const DEFAULT_EMAIL_HEADER = { triggerAutoResponseEmail: false, triggerOtherEmail: false, triggerUserEmail: false };
 
 export default class FlowDmlOptions extends LightningElement {
-    @api value;
+	@api value;
 
-    get safeValue() {
-        return this.value ?? {};
-    }
+	get safeValue() {
+		return this.value ?? {};
+	}
 
-    get assignmentRule() {
-        return this.safeValue.assignmentRuleHeader ?? DEFAULT_ASSIGNMENT_RULE;
-    }
+	get assignmentRule() {
+		return this.safeValue.assignmentRuleHeader ?? DEFAULT_ASSIGNMENT_RULE;
+	}
 
-    get duplicateRule() {
-        return this.safeValue.duplicateRuleHeader ?? DEFAULT_DUPLICATE_RULE;
-    }
+	get duplicateRule() {
+		return this.safeValue.duplicateRuleHeader ?? DEFAULT_DUPLICATE_RULE;
+	}
 
-    get emailHeader() {
-        return this.safeValue.emailHeader ?? DEFAULT_EMAIL_HEADER;
-    }
+	get emailHeader() {
+		return this.safeValue.emailHeader ?? DEFAULT_EMAIL_HEADER;
+	}
 
-    _emit(updated) {
-        this.dispatchEvent(new CustomEvent('dmloptionschange', { detail: { value: updated } }));
-    }
+	_emit(updated) {
+		this.dispatchEvent(new CustomEvent("dmloptionschange", { detail: { value: updated } }));
+	}
 
-    _merge(patch) {
-        this._emit({ ...this.safeValue, ...patch });
-    }
+	_merge(patch) {
+		this._emit({ ...this.safeValue, ...patch });
+	}
 
-    handleText(event) {
-        this._merge({ [event.target.name]: event.target.value });
-    }
+	handleText(event) {
+		this._merge({ [event.target.name]: event.target.value });
+	}
 
-    handleToggle(event) {
-        this._merge({ [event.target.name]: event.target.checked });
-    }
+	handleToggle(event) {
+		this._merge({ [event.target.name]: event.target.checked });
+	}
 
-    handleAssignmentRuleText(event) {
-        this._merge({
-            assignmentRuleHeader: { ...this.assignmentRule, [event.target.name]: event.target.value }
-        });
-    }
+	handleAssignmentRuleText(event) {
+		this._merge({
+			assignmentRuleHeader: { ...this.assignmentRule, [event.target.name]: event.target.value }
+		});
+	}
 
-    handleAssignmentRuleToggle(event) {
-        this._merge({
-            assignmentRuleHeader: { ...this.assignmentRule, [event.target.name]: event.target.checked }
-        });
-    }
+	handleAssignmentRuleToggle(event) {
+		this._merge({
+			assignmentRuleHeader: { ...this.assignmentRule, [event.target.name]: event.target.checked }
+		});
+	}
 
-    handleDuplicateRuleToggle(event) {
-        this._merge({
-            duplicateRuleHeader: { ...this.duplicateRule, [event.target.name]: event.target.checked }
-        });
-    }
+	handleDuplicateRuleToggle(event) {
+		this._merge({
+			duplicateRuleHeader: { ...this.duplicateRule, [event.target.name]: event.target.checked }
+		});
+	}
 
-    handleEmailHeaderToggle(event) {
-        this._merge({
-            emailHeader: { ...this.emailHeader, [event.target.name]: event.target.checked }
-        });
-    }
+	handleEmailHeaderToggle(event) {
+		this._merge({
+			emailHeader: { ...this.emailHeader, [event.target.name]: event.target.checked }
+		});
+	}
 }
--- src/plugins/flow-actions/lwc/flowDmlSection/flowDmlSection.js
+++ src/plugins/flow-actions/lwc/flowDmlSection/flowDmlSection.js
@@ -1,24 +1,24 @@
-import { LightningElement, api, track } from 'lwc';
+import { LightningElement, api, track } from "lwc";
 
 export default class FlowDmlSection extends LightningElement {
-    @api label;
-    @api isRequired = false;
+	@api label;
+	@api isRequired = false;
 
-    @track _isExpanded;
+	@track _isExpanded;
 
-    connectedCallback() {
-        this._isExpanded = this.isRequired;
-    }
+	connectedCallback() {
+		this._isExpanded = !!this.isRequired && this.isRequired !== 'false';
+	}
 
-    get isExpanded() {
-        return this._isExpanded;
-    }
+	get isExpanded() {
+		return this._isExpanded;
+	}
 
-    get chevronIcon() {
-        return this._isExpanded ? 'utility:chevrondown' : 'utility:chevronright';
-    }
+	get chevronIcon() {
+		return this._isExpanded ? "utility:chevrondown" : "utility:chevronright";
+	}
 
-    handleToggle() {
-        this._isExpanded = !this._isExpanded;
-    }
+	handleToggle() {
+		this._isExpanded = !this._isExpanded;
+	}
 }
